0e148ae8d22c4994d94bc7b6139af928de135fcd
tesla59 <nishant@heim.id>
2024-08-05 02:33:44 +0530

    init repo
    Signed-off-by: tesla59 <nishant@heim.id>

---
+++ src/main/java/com/tesla59/dismanisinnoconditiontofuckasheep/DismanisinnoconditiontofuckasheepApplication.java
@@ -0,0 +1,13 @@
+package com.tesla59.dismanisinnoconditiontofuckasheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DismanisinnoconditiontofuckasheepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DismanisinnoconditiontofuckasheepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesla59/dismanisinnoconditiontofuckasheep/DismanisinnoconditiontofuckasheepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesla59.dismanisinnoconditiontofuckasheep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DismanisinnoconditiontofuckasheepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
