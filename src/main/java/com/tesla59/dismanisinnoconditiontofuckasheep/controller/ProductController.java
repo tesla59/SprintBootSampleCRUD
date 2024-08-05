50a99ce916ce53f38c8facf1fd116b0827fd5b88
tesla59 <nishant@heim.id>
2024-08-05 17:22:02 +0530

    products: Post endpoint
    Signed-off-by: tesla59 <nishant@heim.id>

--- src/main/java/com/tesla59/dismanisinnoconditiontofuckasheep/controller/ProductController.java
+++ src/main/java/com/tesla59/dismanisinnoconditiontofuckasheep/controller/ProductController.java
@@ -3,8 +3,11 @@ package com.tesla59.dismanisinnoconditiontofuckasheep.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tesla59.dismanisinnoconditiontofuckasheep.service.ProductService;
@@ -15,13 +18,18 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<ProductModel> getProducts() {
         return productService.getProducts();
     }
 
-    @RequestMapping("/products/{ID}")
+    @GetMapping("/products/{ID}")
     public ProductModel getProductByID(@PathVariable int ID) {
         return productService.getProductByID(ID);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@Validated @RequestBody ProductModel product) {
+        productService.addProduct(product);
     }
 }
--- src/main/java/com/tesla59/dismanisinnoconditiontofuckasheep/service/ProductService.java
+++ src/main/java/com/tesla59/dismanisinnoconditiontofuckasheep/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.tesla59.dismanisinnoconditiontofuckasheep.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,9 +9,9 @@ import com.tesla59.dismanisinnoconditiontofuckasheep.model.ProductModel;;
 
 @Service
 public class ProductService {
-    private List<ProductModel> ProductList = Arrays.asList(
+    private List<ProductModel> ProductList = new ArrayList<>(Arrays.asList(
             new ProductModel(1, "IPhone", 100),
-            new ProductModel(2, "Samsung", 200));
+            new ProductModel(2, "Samsung", 200)));
 
     public List<ProductModel> getProducts() {
         return ProductList;
@@ -20,5 +21,9 @@ public class ProductService {
         return ProductList.stream()
                 .filter(p -> p.getID() == ID)
                 .findFirst().orElse(new ProductModel(-1, "Item Not Found", 0));
+    }
+
+    public void addProduct(ProductModel product) {
+        ProductList.add(product);
     }
 }
